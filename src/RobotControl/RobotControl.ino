5e2746931bb2edd1670b65175fa160de779d6efb
larratic <lauren_milliken@yahoo.com>
2016-05-24 13:26:46 -0700

    Add Start Button

--- src/RobotControl/RobotControl.ino
+++ src/RobotControl/RobotControl.ino
@@ -9,163 +9,55 @@ int x;
 Servo s1;
 Servo s2;
 
+int limit1 = 13;
+int limit2 = 3;
+int limit3 = 4;
+int limit4 = 5;
+
+int step1Dir = 6;
+int step1Step = 7;
+int step2Dir = 8;
+int step2Step = 9;
+
+int enablePin = 10;
+
+int servo1 = 11;
+int servo2 = 12;
+
+int startButton = 2;
+
+boolean motorsOff = HIGH;
+
+
 void setup() 
 {
   Serial.begin(BAUD);
-  pinMode(6,OUTPUT); // Enable
-  pinMode(5,OUTPUT); // Step
-  pinMode(4,OUTPUT); // Dir
-  digitalWrite(6,LOW); // Set Enable low
+  pinMode(enablePin,OUTPUT); // Enable
+  pinMode(step1Step,OUTPUT); // Step
+  pinMode(step1Dir,OUTPUT); // Dir
+  digitalWrite(enablePin,LOW); // Set Enable low
   
-  pinMode(10,OUTPUT); // Enable
-  pinMode(9,OUTPUT); // Step
-  pinMode(8,OUTPUT); // Dir
+  pinMode(enablePin,OUTPUT); // Enable
+  pinMode(step2Step,OUTPUT); // Step
+  pinMode(step2Dir,OUTPUT); // Dir
   digitalWrite(10,LOW); // Set Enable low
   
-  pinMode(2,INPUT); //Limit switch1
-  pinMode(12,INPUT);
-  pinMode(13,INPUT); 
+  pinMode(limit1,INPUT); //Limit switch1
+  pinMode(limit2,INPUT);
+  pinMode(limit3,INPUT); 
+  pinMode(limit4,INPUT); 
   
-  attachInterrupt(2,stopMotors,HIGH);
-  s1.attach(12);
-  s2.attach(13);
+  s1.attach(servo1);
+  s2.attach(servo2);
+  
+  pinMode(startButton,INPUT); //Start Button
+  attachInterrupt(0,stopMotors, CHANGE);
   
   
   
 }
 
-void stopMotors(){
-  Serial.println("STOP");
-}
 
-void posX(int steps){
-  digitalWrite(4,LOW); 
-  digitalWrite(8,LOW);// Set Dir high
-  for(x = 0; x < steps; x++) // Loop step times
-  {
-    digitalWrite(5,HIGH); 
-    digitalWrite(9,HIGH);// Output high
-    delay(1); // Wait
-    digitalWrite(5,LOW); 
-    digitalWrite(9,LOW);// Output low
-    delay(1); // Wait
-  } 
-}
-
-void negX(int steps){
-  digitalWrite(4,HIGH); 
-  digitalWrite(8,HIGH);// Set Dir high
-  for(x = 0; x < steps; x++) // Loop step times
-  {
-    digitalWrite(5,HIGH); 
-    digitalWrite(9,HIGH);// Output high
-    delay(1); // Wait
-    digitalWrite(5,LOW); 
-    digitalWrite(9,LOW);// Output low
-    delay(1); // Wait
-  } 
-}
-
-void posY(int steps){
-  digitalWrite(4,HIGH); 
-  digitalWrite(8,LOW);// Set Dir high
-  for(x = 0; x < steps; x++) // Loop step times
-  {
-    digitalWrite(5,HIGH); 
-    digitalWrite(9,HIGH);// Output high
-    delay(1); // Wait
-    digitalWrite(5,LOW); 
-    digitalWrite(9,LOW);// Output low
-    delay(1); // Wait
-  } 
-}
-
-void negY(int steps){
-  digitalWrite(4,LOW); 
-  digitalWrite(8,HIGH);// Set Dir high
-  for(x = 0; x < steps; x++) // Loop step times
-  {
-    digitalWrite(5,HIGH); 
-    digitalWrite(9,HIGH);// Output high
-    delay(1); // Wait
-    digitalWrite(5,LOW); 
-    digitalWrite(9,LOW);// Output low
-    delay(1); // Wait
-  } 
-}
-
-void initialize(){
-  digitalWrite(4,HIGH); 
-  digitalWrite(8,HIGH);// Set Dir high
-  int i = 0;
-  while(i==0){
-    
-    if(digitalRead(2)){
-     
-      digitalWrite(5,HIGH); 
-      digitalWrite(9,HIGH);// Output high
-      delay(1); // Wait
-      digitalWrite(5,LOW); 
-      digitalWrite(9,LOW);// Output low
-      delay(1); // Wait
-    }
-    else{
-     i=1;
-   }
-  
-  }
-  Serial.println("StOP");
-  
-  digitalWrite(4,LOW); 
-  digitalWrite(8,LOW);// Set Dir high
-  for(x = 0; x < 100; x++) // Loop step times
-  {
-    digitalWrite(5,HIGH); 
-    digitalWrite(9,HIGH);// Output high
-    delay(1); // Wait
-    digitalWrite(5,LOW); 
-    digitalWrite(9,LOW);// Output low
-    delay(1); // Wait
-    
-  } 
-  
-  delay(1000);
-  digitalWrite(4,LOW);
-  digitalWrite(8,HIGH);
-  i = 0;
-  
-  while(i == 0){
-    
-    if(digitalRead(11)){
-      digitalWrite(5,HIGH); 
-      digitalWrite(9,HIGH);// Output high
-      delay(1); // Wait
-      digitalWrite(5,LOW); 
-      digitalWrite(9,LOW);// Output low
-      delay(1); // Wait
-    }
-    else{
-     i = 1;
-   }
-  
-  }
-  Serial.println("StOP");
-  
-  digitalWrite(4,HIGH); 
-  digitalWrite(8,LOW);// Set Dir high
-  for(x = 0; x < 100; x++) // Loop step times
-  {
-    digitalWrite(5,HIGH); 
-    digitalWrite(9,HIGH);// Output high
-    delay(1); // Wait
-    digitalWrite(5,LOW); 
-    digitalWrite(9,LOW);// Output low
-    delay(1); // Wait
-    
-  } 
-  delay(1000);
-  
-}
 
 
 boolean I = true;
@@ -179,10 +71,13 @@ boolean stringComplete = false;
 
 // 300 steps = 2 in
 // 150 steps = 1 in
+int stepsPerX = 150;
+int stepsPerY = 150;
+
+float dropOff[] = {3.000,5.000};
 
 void loop() {
-  digitalWrite(6,LOW); 
-  digitalWrite(10,LOW);// Set Enable low
+  digitalWrite(enablePin,LOW);// Set Enable low
 
     if (I){
       Serial.println("INTIALIZING");
@@ -192,15 +87,12 @@ void loop() {
  delay(1000);
  posX(200);
  
- Xposition = 2.000;
- Yposition = 2.000;
- 
- 
- 
- 
+ Xposition = 300.000/stepsPerX;
+ Yposition = 300.000/stepsPerY;
    
  }
- while(!stringComplete){
+
+ while(!stringComplete and !motorsOff ){
  if(Serial.available()){ // only send data back if data has been sent
     char inByte = Serial.read(); // read the incoming data
     
@@ -218,7 +110,7 @@ void loop() {
   if(inputString != ""){
     Serial.println(inputString);
     char command = inputString[0];
-    if (command == '0'){
+    if (command == '0'){ //GOTO
      String X = inputString.substring(2,7);
      String Y = inputString.substring(8,13);
      char floatbuf[32]; // make this at least big enough for the whole string
@@ -226,13 +118,34 @@ void loop() {
       float x = atof(floatbuf);
      Y.toCharArray(floatbuf, 32);
       float y = atof(floatbuf);
-      Serial.write("OK");
-      
+   
       goTo(x,y,Xposition,Yposition);
       Xposition = x;
       Yposition = y;
+      Serial.write("OK");
 
     }
+    if (command == '1') { //Calibration
+      initialize();
+      Xposition = 100.0/stepsPerX;
+      Yposition = 100.0/stepsPerY;
+      Serial.write("OK");
+    }
+    
+    if (command == '2') { //Pick up a Fish
+      goFishing();
+      Serial.write("OK");
+    }
+    
+    if (command == '3') { // Deposit Fish
+      goTo(dropOff[0],dropOff[1],Xposition,Yposition);
+      Xposition = dropOff[0];
+      Yposition = dropOff[1];
+      goFishing();
+      Serial.write("OK");
+    }
+    
+    
   }
   inputString = "";
   stringComplete = false;
@@ -240,10 +153,14 @@ void loop() {
 
   
 }
+//Serial.println("FINISHED THE GAME");
+//}
 
+
+// GOTO command
 void goTo(float moveX,float moveY,float currX, float currY){
-  int stepsX = (moveX - currX)*150;
- int stepsY = (moveY - currY)*150;
+  int stepsX = (moveX - currX)*stepsPerX;
+ int stepsY = (moveY - currY)*stepsPerY;
  
  
  if (stepsX < 0){
@@ -267,4 +184,155 @@ void goTo(float moveX,float moveY,float currX, float currY){
    Serial.println(stepsY);
   posY(stepsY); 
  }
+};
+
+//Pick up a fish
+void goFishing(){
+   for(int ii = 20; ii <90; ii++){
+    s1.write(ii);
+    delay(5);
+   }
+
+  delay(1000);
+  for(int ii = 90; ii >20; ii--){
+    s1.write(ii);
+    delay(10);
+   }
+  
+};
+
+void stopMotors(){
+  motorsOff = !motorsOff;
+  digitalWrite(enablePin,motorsOff);
+
+}
+
+void posX(int steps){
+  digitalWrite(step1Dir,LOW); 
+  digitalWrite(step2Dir,LOW);// Set Dir 
+  for(x = 0; x < steps; x++) // Loop step times
+  {
+    digitalWrite(step1Step,HIGH); 
+    digitalWrite(step2Step,HIGH);// Output high
+    delay(1); // Wait
+    digitalWrite(step1Step,LOW); 
+    digitalWrite(step2Step,LOW);// Output low
+    delay(1); // Wait
+  } 
+}
+
+void negX(int steps){
+  digitalWrite(step1Dir,HIGH); 
+  digitalWrite(step2Dir,HIGH);// Set Dir high
+  for(x = 0; x < steps; x++) // Loop step times
+  {
+    digitalWrite(step1Step,HIGH); 
+    digitalWrite(step2Step,HIGH);// Output high
+    delay(1); // Wait
+    digitalWrite(step1Step,LOW); 
+    digitalWrite(step2Step,LOW);// Output low
+    delay(1); // Wait
+  } 
+}
+
+void posY(int steps){
+  digitalWrite(step1Dir,HIGH); 
+  digitalWrite(step2Dir,LOW);// Set Dir high
+  for(x = 0; x < steps; x++) // Loop step times
+  {
+    digitalWrite(step1Step,HIGH); 
+    digitalWrite(step2Step,HIGH);// Output high
+    delay(1); // Wait
+    digitalWrite(step1Step,LOW); 
+    digitalWrite(step2Step,LOW);// Output low
+    delay(1); // Wait
+  } 
+}
+
+void negY(int steps){
+  digitalWrite(step1Dir,LOW); 
+  digitalWrite(step2Dir,HIGH);// Set Dir high
+  for(x = 0; x < steps; x++) // Loop step times
+  {
+    digitalWrite(step1Step,HIGH); 
+    digitalWrite(step2Step,HIGH);// Output high
+    delay(1); // Wait
+    digitalWrite(step1Step,LOW); 
+    digitalWrite(step2Step,LOW);// Output low
+    delay(1); // Wait
+  } 
+}
+
+
+//Initialization 
+void initialize(){
+  digitalWrite(step1Dir,HIGH); 
+  digitalWrite(step2Dir,HIGH);// Set Dir high
+  int i = 0;
+  while(i==0){
+    
+    if(digitalRead(limit1)){
+     
+      digitalWrite(step1Step,HIGH); 
+      digitalWrite(step2Step,HIGH);// Output high
+      delay(1); // Wait
+      digitalWrite(step1Step,LOW); 
+      digitalWrite(step2Step,LOW);// Output low
+      delay(1); // Wait
+    }
+    else{
+     i=1;
+   }
+  
+  }
+  Serial.println("StOP");
+  
+  digitalWrite(step1Dir,LOW); 
+  digitalWrite(step2Dir,LOW);// Set Dir high
+  for(x = 0; x < 100; x++) // Loop step times
+  {
+    digitalWrite(step1Step,HIGH); 
+    digitalWrite(step2Step,HIGH);// Output high
+    delay(1); // Wait
+    digitalWrite(step1Step,LOW); 
+    digitalWrite(step2Step,LOW);// Output low
+    delay(1); // Wait
+    
+  } 
+  
+  delay(1000);
+  digitalWrite(step1Dir,LOW);
+  digitalWrite(step2Dir,HIGH);
+  i = 0;
+  
+  while(i == 0){
+    
+    if(digitalRead(limit2)){
+      digitalWrite(step1Step,HIGH); 
+      digitalWrite(step2Step,HIGH);// Output high
+      delay(1); // Wait
+      digitalWrite(step1Step,LOW); 
+      digitalWrite(step2Step,LOW);// Output low
+    }
+    else{
+     i = 1;
+   }
+  
+  }
+  Serial.println("StOP");
+  
+  digitalWrite(step1Dir,HIGH); 
+  digitalWrite(step2Dir,LOW);// Set Dir high
+  for(x = 0; x < 100; x++) // Loop step times
+  {
+    digitalWrite(step1Step,HIGH); 
+    digitalWrite(step2Step,HIGH);// Output high
+    delay(1); // Wait
+    digitalWrite(step1Step,LOW); 
+    digitalWrite(step2Step,LOW);// Output low
+    delay(1); // Wait
+    
+  } 
+  delay(1000);
+  
 };
